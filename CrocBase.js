61a1edfa876c29031ea86bce68155e4d41b45564
Kieron Gillespie <kieron.gillespie@anaren.com>
2017-09-28 18:35:29 -0400

    Fixing the removeChild function in CrocBase.

--- CrocBase.js
+++ CrocBase.js
@@ -125,13 +125,22 @@ CrocBase.prototype.addChildren = function(uiObjectList) {
 };
 
 CrocBase.prototype.removeChild = function(uiObject) {
+
+	var childIndex = -1;
 	
-	if(!(uiObject in this.children)) {
+	for(var i = 0; i < this.children.length; i++) {
+		if(this.children[i].uuid === uiObject.uuid) {
+			childIndex = i;
+			break;
+		}
+	}
+	
+	if(childIndex < 0) {
 		console.log("CrocBase.prototype.removeChild: Has no UIObject!");
 		return false;
 	}
 	
-	this.children.splice(this.children.index(uiObject));
+	this.children.splice(childIndex);
 	
 	this.getRoot().repaint();
 	
