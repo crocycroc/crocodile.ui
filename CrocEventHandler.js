4528bbc331430c756b9ae7697710a31204586d1c
Kieron Gillespie <ciaran.gillespie@gmail.com>
2017-10-03 11:07:41 -0400

    We only care to lock focus on some event propigation not all.

--- CrocEventHandler.js
+++ CrocEventHandler.js
@@ -76,8 +76,8 @@ CrocEventHandler.prototype.onCanvasResize = function() {
 	this.root.repaint();
 };
 
-CrocEventHandler.prototype.sendHitEvent = function(coords, eventType) {
-	if(this.root.focusedObject !== null) {
+CrocEventHandler.prototype.sendHitEvent = function(coords, eventType, focused) {
+	if(this.root.focusedObject !== null && focused) {
 		if(this.root.focusedObject.event(eventType, coords)) {
 			var hits = this.root.hitTest(coords.x, coords.y);
 			if(this.propagateHitEvent(hits, eventType, coords) === true) {
